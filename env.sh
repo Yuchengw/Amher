38193415e713283ad5dac8d96545a7c39a26c99d
yuchengw <ycwmike@gmail.com>
2015-02-24 23:10:36 -0800

    add linux setting up stuff

---
+++ env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#Source this file in your termial to get a working env set up for you
+#
+#@copy Amher.inc
+####
+
+##Check that user is set
+
+WARNING="****************WARNING***********"
+
+if [ -z "${USER:-}" ]; then
+	export USER=$(whoami)
+	echo "$WARING"
+	echo "Environment variable USER was not set, Set to $USER using whoami"
+fi
+
+function set_amher_ENV () {
+	export AMHER_HOME="$1"
+	export WEB_HOME="$AMHER_HOME/web"
+	export WEB_TOOLS_HOME="$WEB_HOME/tools"
+	export WEB_JDK_HOME="$WEB_TOOLS_HOME/jdk"
+	#TODO: select the correct jdk home according to the os version
+	export JAVA_HOME="$WEB_JDK_HOME/Linux/$2"
+	export PATH="$1:$JAVA_HOME/bin"
+
+	if [ -f $(uname -s) = Darwin ]; then
+		export JAVA_HOME="$WEB_JDK_HOME/Mac/$2"
+	fi
+}
+
+set_amher_ENV $HOME/Amher jdk1.7.0_80
